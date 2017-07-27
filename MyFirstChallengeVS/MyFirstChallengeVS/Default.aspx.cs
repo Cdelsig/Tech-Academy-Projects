ff2c0d909ab340ccc0807c3b5f0433894c8e9432
Carrie D <cdelsig@gmail.com>
2017-07-27 16:21:03 -0700

    Exploring VS and C#

---
+++ MyFirstChallengeVS/MyFirstChallengeVS/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallengeVS
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ageTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void goButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string money = moneyTextBox.Text;
+
+            string result = $"At {age} years old, I'm impressed you even carry cash, let alone {money} dollars!";
+
+            goLabel.Text = result;
+        }
+    }
+}
---
+++ MyVSWebApp/MyVSWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyVSWebApp.Global" Language="C#" %>
