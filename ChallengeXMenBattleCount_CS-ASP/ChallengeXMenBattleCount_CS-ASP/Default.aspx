30db2a79938ea5af153e8ce41c612265f4fa99e1
Carrie D <cdelsig@gmail.com>
2017-08-03 23:11:17 -0700

    Practicing CS-ASP for loop through array

---
+++ ChallengeXMenBattleCount_CS-ASP/ChallengeXMenBattleCount_CS-ASP/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeXMenBattleCount_CS_ASP.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            The most battles belong to:
+            <asp:Label ID="mostLabel" runat="server"></asp:Label>
+            <br />
+            The fewest battles belong to:
+            <asp:Label ID="fewestLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeXMenBattleCount_CS-ASP/ChallengeXMenBattleCount_CS-ASP/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeXMenBattleCount_CS_ASP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Phoenix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            int indexMax = 0;
+            int indexMin = 0;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (numbers[i] > numbers[indexMax])
+                {
+                    indexMax = i;
+                }
+                if (numbers[i] < numbers[indexMin])
+                {
+                    indexMin = i;
+                }
+            }
+            mostLabel.Text = String.Format("{0} (Value: {1})", names[indexMax], numbers[indexMax]);
+            fewestLabel.Text = String.Format("{0} (Value: {1})", names[indexMin], numbers[indexMin]);
+            
+        }
+    }
+}
