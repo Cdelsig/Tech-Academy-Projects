eca5811aee8edb51fdbafe7a690b708cfbc8d384
Carrie D <cdelsig@gmail.com>
2017-08-09 22:16:43 -0700

    Practicing CS-ASP string manipulation

---
+++ ChallengeFunWithStrings_CS-ASP/ChallengeFunWithStrings_CS-ASP/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeFunWithStrings_CS_ASP.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Fun With Strings Challenge!!</h1>
+            <br />
+            <br />
+            Reverse &quot;Carrie Del Signore&quot;: <asp:Label ID="reverseNameLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Reverse sequence &quot;Luke, Leia, Han, Chewbacca&quot;:
+            <asp:Label ID="reverseStarWarsLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Use padding to make each name the same length:<br />
+            &quot;Luke, Leia, Han, Chewbacca&quot;<br />
+            <br />
+            <asp:Label ID="padLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Fix the spelling errors in this string:<br />
+            &quot;NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.&quot;<br />
+            <br />
+            <asp:Label ID="removeLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            ready???<br />
+            <br />
+&nbsp;<asp:Button ID="goButton" runat="server" OnClick="goButton_Click" Text="Go!" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeFunWithStrings_CS-ASP/ChallengeFunWithStrings_CS-ASP/Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeFunWithStrings_CS_ASP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void goButton_Click(object sender, EventArgs e)
+        {
+            // 1.  Reverse your name
+            string name = "Carrie Del Signore";
+            string reverseName = "";
+
+            for (int i = name.Length - 1; i >= 0; i--)
+                {
+                    reverseName += name[i].ToString();
+                }
+            reverseNameLabel.Text = reverseName;
+
+
+            // 2.  Reverse this sequence:
+            string swNames = "Luke,Leia,Han,Chewbacca";
+            // When you're finished it should look like this:
+            // Chewbacca,Han,Leia,Luke
+            string[] tempString = swNames.Split(',');
+            string reverseSWnames = "";
+
+            for (int i = tempString.Length - 1; i >= 0; i--)
+            {
+                reverseSWnames += tempString[i] + ", ".ToString();
+            }
+            reverseStarWarsLabel.Text = reverseSWnames;
+
+
+            // 3. Use the sequence to create ascii art (14):
+            // *****luke*****
+            // *****leia*****
+            // *****han******
+            // **Chewbacca***
+
+            string[] myArray = { "Luke", "Leia", "Han", "Chewbacca!" };
+            string asciiArt = "";
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                int padLeft = (14 - myArray[i].Length) / 2;
+                string tempStr = myArray[i].PadLeft(myArray[i].Length + padLeft, '*');
+
+                asciiArt += tempStr.PadRight(14, '*') + "<br />";
+            }
+            padLabel.Text = asciiArt;
+
+
+
+            // 4. Solve this puzzle:
+
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+            string newPuzzle = "";
+            string capPuzzle = "";
+            // Once you fix it with string helper methods, it should read:
+            // Now is the time for all good men to come to the aid of their country.
+
+            int index = puzzle.IndexOf("remove-me");
+            newPuzzle = puzzle.Remove(index, 9).Replace("Z", "T").ToLower();
+            capPuzzle = newPuzzle.Remove(0, 1).Insert(0, "N");
+
+            removeLabel.Text = capPuzzle;
+        }
+    }
+}
